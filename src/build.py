2d90da1e7eeb8e9072617de42077496d31d9bcaa
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-12-08 21:13:55 +0200

    Create build.py

---
+++ src/build.py
@@ -0,0 +1,20 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.optionxform = str
+config.read('../../../config.ini')
+
+bases = config['BASES']
+keys = bases.keys()
+
+baseFolders = []
+
+for b in keys:
+    baseFolders.append(bases.get(b))
+
+columns = config['COLUMNS'].keys()
+
+print(type(columns))
+
+for k in columns:
+    print(k)
